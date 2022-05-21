72e662a187a37bed3519b6f012d04dd8f417c110
Jamblefoot <32472250+Jamblefoot@users.noreply.github.com>
2022-05-21 12:31:00 -0700

    Initial commit

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    float xOffset = 0f;
+    float yOffset = 0f;
+
+    public Transform tran;
+    Transform cameraTran;
+
+    Vector3 lastPos;
+    Vector3 cameraTargetLocalPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tran = transform;
+        cameraTran = GetComponentInChildren<Camera>().transform;
+        cameraTargetLocalPos = cameraTran.localPosition;
+
+        lastPos = tran.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xOffset -= Input.GetAxis("Mouse X");
+        yOffset -= Input.GetAxis("Mouse Y");
+        yOffset = Mathf.Clamp(yOffset, -80f, 80f);
+
+        tran.localRotation = Quaternion.Euler(yOffset, xOffset, 0f);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 moveDelta = tran.position - lastPos;
+        lastPos = tran.position;
+        cameraTran.position -= moveDelta;
+        cameraTran.localPosition = Vector3.Lerp(cameraTran.localPosition, cameraTargetLocalPos, Time.deltaTime * 10);
+    }
+
+    public void ChangeXRotation(float change)
+    {
+        xOffset -= change;
+    }
+}
---
+++ Assets/Scripts/GhostDrive.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostDrive : MonoBehaviour
+{
+    FollowCam followCam;
+
+    public float speedMult = 0.1f;
+    public float rotSpeedMult = 5f;
+    float acceleration = 10f;
+    float vertCalced = 0f;
+    float horCalced = 0f;
+    float rotSpeed = 0f;
+
+    float vertical, horizontal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        followCam = FindObjectOfType<FollowCam>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxis("Vertical") * speedMult;
+        vertCalced = Mathf.Lerp(vertCalced, vertical, Time.deltaTime * acceleration);
+        horizontal = Input.GetAxis("Horizontal") * speedMult;
+        horCalced = Mathf.Lerp(horCalced, horizontal, Time.deltaTime * acceleration);
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 move = Vector3.zero;
+
+        if(followCam != null)
+        {
+            //move followcam forward
+            Vector3 forward = Vector3.ProjectOnPlane(followCam.tran.forward, Vector3.up).normalized;
+            move = forward * vertCalced + followCam.tran.right * horCalced;
+            if(Mathf.Abs(vertical) > 0.01f)
+            {
+                rotSpeed = Mathf.Lerp(rotSpeed, rotSpeedMult, Time.deltaTime * acceleration * 0.5f);
+                Vector3 rot = Vector3.RotateTowards(transform.forward, forward * Mathf.Sign(vertCalced), Time.deltaTime * rotSpeed, 0);
+                float rotChange = Vector3.SignedAngle(transform.forward, rot, transform.up);
+                transform.LookAt(transform.position + rot, transform.up);
+                followCam.ChangeXRotation(rotChange);
+            }
+            else
+            {
+                rotSpeed = 0f;
+            }
+        }
+        else
+        {
+            move = transform.forward * vertCalced + transform.right * horCalced;
+        }
+
+        transform.position += move;
+    }
+}
